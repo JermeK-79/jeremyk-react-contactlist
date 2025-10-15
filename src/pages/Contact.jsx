2f0daf0f44d7ad19dcb000716bde434491aeb2f1
Jeremy Keene <Jeremy496@mac.com>
2025-10-15 01:08:16 +0000

    edited Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,21 +1,21 @@
 import { Link } from "react-router-dom";
 import { ContactCard } from "../components/ContactCard";
-import useGlobalReducer from "../hooks/useGlobalReducer"
+import useGlobalReducer from "../hooks/useGlobalReducer";
 import { useEffect } from "react";
 import { fetchAllContacts } from "../lib/fetch";
 
 export const Contact = () => {
     const {store, dispatch} = useGlobalReducer();
-
-    useEffect (() => {
+    
+    useEffect(() => {
         fetchAllContacts(dispatch);
-    }, [] )
+    }, [dispatch]);
     
     return(
         <>
             <div className="container">
             {
-                !store && !store.contacts
+                !store || !store.contacts || store.contacts.length === 0
                 ?
                 <h1>Loading...</h1>
                 :
@@ -23,10 +23,10 @@ export const Contact = () => {
                     return(
                         <div className="card d-inline-flex flex-row" key={contact.id}>
                             <ContactCard 
-                            name={contact.name}
-                            address={contact.address}
-                            phone={contact.phone}
-                            email={contact.email} 
+                                name={contact.name}
+                                address={contact.address}
+                                phone={contact.phone}
+                                email={contact.email} 
                             />
                             <button>Edit</button>
                             <button>Delete</button>
